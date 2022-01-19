9cfbf7cc99706cd3c37307cb0263bb665fd5276d
micha vie <micha@freeiam.com>
2022-01-19 01:59:06 +0100

    add token to user ids dist script

--- scripts/distribute-tokens-to-user-ids.ts
+++ scripts/distribute-tokens-to-user-ids.ts
@@ -18,51 +18,42 @@ import {
   BytesValue,
 } from '@elrondnetwork/erdjs'
 
-const ContestTweetId = ''
 const RewardTokenIdentifier = ''
 const RewardTokenAmount = 0
+const InputFile = 'in.json'
 const PemFile = 'distributor.pem'
-const IsTestnet = true
+const IsTestnet = false // no practical usernames on testnet
 const SimulateSending = true
-const ControlPanelApiBaseUrl = IsTestnet ? 'http://superciety--cp.test/api' : 'https://cp.superciety.com/api'
-const ControlPanelApiAuthToken = 'IF YOU CHECK THIS IN YOU ARE A NOOB'
 
-type ApiContestDistributionInfo = {
-  finished: boolean
-  count: number
-  addresses: string[]
-}
+const ProxyUrl = IsTestnet ? 'https://testnet-gateway.elrond.com' : 'https://gateway.elrond.com'
+const ApiUrl = IsTestnet ? 'https://testnet-api.elrond.com' : 'https://api.elrond.com'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const main = async () => {
-  const proxyUrl = IsTestnet ? 'https://testnet-gateway.elrond.com' : 'https://gateway.elrond.com'
-  const provider = new ProxyProvider(proxyUrl, { timeout: 5000 })
+  const provider = new ProxyProvider(ProxyUrl, { timeout: 5000 })
   const signer = await getSigner()
   const account = new Account(signer.getAddress())
-  const distributionInfo = await fetchContestDistributionInfo()
+  const winners = await getWinnerIdentifiers()
 
-  if (!distributionInfo) {
-    console.error('failed to get contest distribution info')
-    return
-  }
-
-  if (!distributionInfo.finished) {
-    console.error('contest has not finished yet.')
-    return
-  }
-
-  console.log(`distributing to ${distributionInfo.count} receivers ...`)
-  console.log(`total distribution value: ${distributionInfo.count * RewardTokenAmount} ${RewardTokenIdentifier}`)
+  console.log(`distributing to ${winners.length} receivers ...`)
+  console.log(`total distribution value: ${winners.length * RewardTokenAmount} ${RewardTokenIdentifier}`)
 
   await NetworkConfig.getDefault().sync(provider)
   await account.sync(provider)
 
   let count = 0
 
-  for (let receiver of distributionInfo.addresses) {
+  for (let receiverIdentifier of winners) {
     count++
-    const tx = await buildRewardTransactionFor(receiver)
+    const address = isAddress(receiverIdentifier) ? receiverIdentifier : await getAddressFromUsername(receiverIdentifier)
+
+    if (!address) {
+      console.error(`failed to fetch address for username '${receiverIdentifier}'`)
+      continue
+    }
+
+    const tx = await buildRewardTransactionFor(address)
     tx.setNonce(account.nonce)
     await signer.sign(tx)
     account.incrementNonce()
@@ -73,12 +64,18 @@ const main = async () => {
       await tx.send(provider)
     }
 
-    console.log(`${count}. sent ${RewardTokenAmount} ${RewardTokenIdentifier} to ${receiver}: ${tx.getHash()}`)
+    console.log(`${count}. sent ${RewardTokenAmount} ${RewardTokenIdentifier} to ${address}: ${tx.getHash()}`)
 
-    await new Promise(r => setTimeout(r, 300)) // ~3 txs / s = ~18 txs / block, in a 6 second block
+    await new Promise(r => setTimeout(r, 300)) // 3 txs / s = 18 txs / block, in a 6 second block
   }
 
   console.log('guess what? we are done!')
+}
+
+const getWinnerIdentifiers = async () => {
+  const inputPath = path.join(__dirname, '..', 'data', InputFile)
+  const inputContents = await fs.promises.readFile(inputPath, { encoding: 'utf8' })
+  return JSON.parse(inputContents)
 }
 
 const getSigner = async () => {
@@ -87,13 +84,14 @@ const getSigner = async () => {
   return UserSigner.fromPem(pemWalletContents)
 }
 
-const fetchContestDistributionInfo = async () => {
-  const res = await fetch(`${ControlPanelApiBaseUrl}/twitter/contests/${ContestTweetId}/distribution/all`, {
-    headers: { Authorization: `Bearer ${ControlPanelApiAuthToken}` },
-  })
+const isAddress = (val: string) => val.startsWith('erd1') && val.length === 62
+
+const getAddressFromUsername = async (username: string) => {
+  const sanitizedUsername = username.replace('@', '').split('.')[0]
+  const res = await fetch(`${ApiUrl}/usernames/${sanitizedUsername}`)
   if (!res.ok) return null
   const body = (await res.json()) as any
-  return body.data as ApiContestDistributionInfo
+  return body.address || null
 }
 
 const buildRewardTransactionFor = async (receiverAddress: string) =>
