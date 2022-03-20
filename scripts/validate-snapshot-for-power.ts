8ce02a9aa2f6e27cc778fc086cf201043d5d6005
micha vie <micha@freeiam.com>
2022-03-20 20:51:03 +0100

    filter uniques in power validation script

--- scripts/validate-snapshot-for-power.ts
+++ scripts/validate-snapshot-for-power.ts
@@ -1,6 +1,8 @@
 import path, { dirname } from 'path'
 import fs from 'fs'
+import fetch from 'node-fetch'
 import { fileURLToPath } from 'url'
+import collect from 'collect.js'
 
 const InputFile = 'snapshot.json'
 const OutputFile = 'snapshot.json'
@@ -10,7 +12,9 @@ const ScyApiUrl = 'https://api.superciety.com'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const main = async () => {
-  const candidates = await getCandidates()
+  const candidates = collect(await getCandidates())
+    .unique()
+    .all()
 
   let count = 0
   let addressesPassed: string[] = []
